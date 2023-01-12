fd1f3bf2a6b59313d50cd3ed26bcd092608dda59
Karsten <99371148+KarstenCole@users.noreply.github.com>
2023-01-12 13:03:45 -0500

    Add files via upload

---
+++ backend_http/node_http.js
@@ -0,0 +1,16 @@
+const http = require("http");
+
+const server = http.createServer((req,res) => {
+    if(req.url === '/'){
+        res.write('Hello world ');
+        res.write("this is our first server");
+        res.end();
+    }
+    if(req.url === '/api/courses'){
+        res.write(JSON.stringify([1,2,3]));
+        res.write("this is a list of offerings");
+        res.end();
+    }
+})
+server.listen(3000);
+console.log("Listening on port 3000 ...");
